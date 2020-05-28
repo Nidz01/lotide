3b8f9ccf53a3ea49ea89dfe6875af96d388fe03e
Nida Q <nidaemailid@gmail.com>
2020-05-28 00:17:49 +0000

    assertion

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  const pass = ':hourglass::hourglass::hourglass::hourglass:';
+  const fail = ':octagonal_sign::octagonal_sign::octagonal_sign:';
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+assertEqual("Nida", "Nida");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 5);
