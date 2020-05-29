2bea5429ea0395a0eca4f6ab136de0ac53a3c5ce
Nida Q <nidaemailid@gmail.com>
2020-05-29 21:54:57 +0000

    middle

---
+++ middle.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log(`✅✅✅ There is no middle: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(array) {
+  if (array.length < 3) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    let firstMiddleValue = array[Math.floor(array.length / 2) - 1];
+    let secondMiddleValue = array[Math.floor(array.length / 2)];
+    return [firstMiddleValue, secondMiddleValue];
+  } else {
+    let middleIndex = array[Math.floor(array.length / 2)];
+    return [middleIndex];
+  }
+}
+
