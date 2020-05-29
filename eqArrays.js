2dc985a09d754cc046a2c3f6b8454fa4724eadfd
Nida Q <nidaemailid@gmail.com>
2020-05-29 17:05:30 +0000

    array assertion with two functions

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  const pass = ':hourglass::hourglass::hourglass::hourglass:';
+  const fail = ':octagonal_sign::octagonal_sign::octagonal_sign:';
+  if (actual === expected) {
+    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+/*TEST CODE
+assertEqual("Nida", "Nida");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 5);
+*/
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
+/*
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+*/
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+
+
