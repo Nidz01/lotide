87de84f9c456b8942d8b77bd5272273b365e0501
Nida Q <nidaemailid@gmail.com>
2020-06-02 17:26:19 +0000

    changes

---
+++ index.js
@@ -0,0 +1,33 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const takeUntil = require('./takeUntil')
+const without = require('./without');
+const map = require('./map');
+const eqObjects = require('./eqObjets');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  takeUntil: takeUntil,
+  without: without,
+  map: map,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions
+};
