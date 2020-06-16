3adce109050c93fc354ceb5e8bd13842c5f0ff68
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-16 13:37:32 -0400

    compare arrays if equal

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
 /*const assertEqual = function(actual, expected) {
-  const pass = ':hourglass::hourglass::hourglass::hourglass:';
-  const fail = ':octagonal_sign::octagonal_sign::octagonal_sign:';
+  const pass = '✅️✅️✅️';
+  const fail = '❌️❌️❌️';
   if (actual === expected) {
     console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -13,7 +13,7 @@ assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(1, 5);
 */
-
+/*Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match. */
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -33,8 +33,8 @@ eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+/*Use assertEqual to write test cases for various scenarios. */
+/*assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
