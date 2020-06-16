aae953bcd26c450c43a3d40ee351321a0b21f8ca
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-16 13:58:42 -0400

    refactored functions with comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,11 @@
 /*
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+The message will be similar to that of assertEqual. In fact, you could refer to the code for assertEqual to help you 
+implement this. Instead of doing a simple === check though, this assertion function will make use of your eqArrays 
+function for array comparison.
+*/
+/*
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -15,7 +22,7 @@ const assertArraysEqual = function(actual, expected) {
   if(eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌️❌️❌️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
