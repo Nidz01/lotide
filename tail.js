87e3662b1df500ae0671d5605a740bca053f33d3
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-15 20:45:23 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,23 +1,36 @@
-/*const assertEqual = function(actual, expected) {
-  const pass = ':hourglass::hourglass::hourglass::hourglass:';
-  const fail = ':octagonal_sign::octagonal_sign::octagonal_sign:';
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
+const assertEqual = function(actual, expected) {
+  const pass = '✅️✅️✅️';
+  const fail = '❌️❌️❌️';
+  if(actual.length === expected.length) {
+    let i=0;
+  while (i < actual.length){
+    if (actual[i] === expected[i]){
+      i++;
+      if(i === actual.length) {
+       console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
+      }
+    }
+  }
+   } else {
     console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 // TEST CODE
-assertEqual("Nida", "Nida");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 5);
+cassertEqual(tail([5,6,7]), 5);
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
 */
 
+/*
+The tail is meant to be every element except the head (first element) of the array.
+Create a function tail which returns the "tail" of an array: everything except for the first item (head) of the provided array.
+Your function should not modify the array that is passed in. It should return a new array.
+*/
 const tail = function(array)
 {
-let string = array[array.length-1];
-return string;
-}
+const newArray = array.slice(1);
+return newArray;
+};
+
 
 module.exports = tail;
