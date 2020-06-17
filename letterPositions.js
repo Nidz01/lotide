d2ce073f2c4af092666e95d0212b4c1274183589
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-17 14:11:00 -0400

    Refactored code

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,6 @@
+/* Function that checks if incoming arrays are equal and
+then return true (if equal) and false(if false) accordingly. */
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -9,15 +12,25 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
+
+/* Function that calls eqArrays function to check if incoming arrays are equal and
+then displays a message accoridngly. */
+
 const assertArraysEqual = function(actual, expected) {
   if(eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌️❌️❌️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+/*function letterPositions which will return all the indices (zero-based positions) 
+in the string where each character is found. For each letter, instead of returning 
+just one number to represent its number of occurrences, multiple numbers may be needed 
+to represent all the places in the string that it shows up.
+*/
 const letterPositions = function(string) {
-  const result = new Object();
+  const result = {};
   for (let index = 0; index < string.length; index++) {
     let char = string[index];
     if (!result[char]) {
@@ -29,44 +42,31 @@ const letterPositions = function(string) {
       result[char].push(index);
     }
   }
-  // Object.keys(result).sort();
   return result;
 }
- const test = {
-   l: [0],
-   i: [1, 11],
-   g: [2],
-   h: [3, 5, 15, 18],
-   t: [4, 14],
-   o: [6, 19],
-   u: [7, 20],
-   s: [8, 21],
-   e: [9, 16, 22],
-   n: [12]
- };
- assertArraysEqual(letterPositions("lighthouse in the house").l, test.l);
- assertArraysEqual(letterPositions("lighthouse in the house").i, test.i);
- assertArraysEqual(letterPositions("lighthouse in the house").g, test.g);
- assertArraysEqual(letterPositions("lighthouse in the house").h, test.h);
- assertArraysEqual(letterPositions("lighthouse in the house").t, test.t);
- assertArraysEqual(letterPositions("lighthouse in the house").o, test.o);
- assertArraysEqual(letterPositions("lighthouse in the house").u, test.u);
- assertArraysEqual(letterPositions("lighthouse in the house").s, test.s);
- assertArraysEqual(letterPositions("lighthouse in the house").e, test.e);
- assertArraysEqual(letterPositions("lighthouse in the house").n, test.n);
- const test2 = {
-   p: [0, 6],
-   i: [1],
-   l: [2],
-   o: [3],
-   t: [4],
-   e: [7],
-   n: [8]
- };
- assertArraysEqual(letterPositions("pilot pen").p, test2.p);
- assertArraysEqual(letterPositions("pilot pen").i, test2.i);
- assertArraysEqual(letterPositions("pilot pen").l, test2.l);
- assertArraysEqual(letterPositions("pilot pen").o, test2.o);
- assertArraysEqual(letterPositions("pilot pen").t, test2.t);
- assertArraysEqual(letterPositions("pilot pen").e, test2.e);
- assertArraysEqual(letterPositions("pilot pen").n, test2.n);
+
+const test = {
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+};
+
+/*TEST CASES*/
+
+assertArraysEqual(letterPositions("lighthouse in the house").l, test.l);
+assertArraysEqual(letterPositions("lighthouse in the house").i, test.i);
+assertArraysEqual(letterPositions("lighthouse in the house").g, test.g);
+assertArraysEqual(letterPositions("lighthouse in the house").h, test.h);
+assertArraysEqual(letterPositions("lighthouse in the house").t, test.t);
+assertArraysEqual(letterPositions("lighthouse in the house").o, test.o);
+assertArraysEqual(letterPositions("lighthouse in the house").u, test.u);
+assertArraysEqual(letterPositions("lighthouse in the house").s, test.s);
+assertArraysEqual(letterPositions("lighthouse in the house").e, test.e);
+assertArraysEqual(letterPositions("lighthouse in the house").n, test.n);
