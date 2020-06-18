eab881d1165f453afc647a018698b2b2ec43c782
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-18 11:56:42 -0400

    Refactored code and function

--- map.js
+++ map.js
@@ -1,3 +1,6 @@
+/* Function that checks if incoming arrays are equal and
+then return true (if equal) and false(if false) accordingly. */
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -9,39 +12,62 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
+
+/*
+Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. */
+
 const assertArraysEqual = function(actual, expected) {
   if(eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌️❌️❌️ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
+/*
+Create a map function that takes two arguments, first "An array to map" and second "A callback function"
+The map function will return a new array based on the results of the callback function.
+*/
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
-    console.log('item BEFORE: ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
   }
   return results;
 }
+//////////////////////////////////////////////////////////
 const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
-// const objects = [{
-//   name: "test",
-//   importance: 1
-// }, {
-//   name: "test1",
-//   importance: 5
-// }];
-//
-// const multiplier = [5, 6, 7, 8];
-// const results2 = map(objects, object => object.name);
-// const results3 = map(multiplier, num => num * 10);
-// console.log(results2);
-//
-// assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
-// assertArraysEqual(results2, ["test", "test1"]);
-// assertArraysEqual(results3, [50, 60, 70, 80]);
+
+/* TEST CASE*/
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+
+//////////////////////////////////////////////////////////
+const objects = [
+  {name: "test",
+  importance: 1
+  }, 
+  {name: "test1",
+  importance: 5
+  }
+];
+  const results2 = map(objects, object => object.name);
+  console.log(results2);
+
+  /* TEST CASE*/
+
+  assertArraysEqual(results2, ["test", "test1"]);
+
+//////////////////////////////////////////////////////////
+const multiplier = [5, 6, 7, 8];
+const results3 = map(multiplier, num => num * 10);
+console.log(results3);
+
+/* TEST CASE*/
+
+assertArraysEqual(results3, [50, 60, 70, 80]);
+//////////////////////////////////////////////////////////
