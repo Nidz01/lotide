3c8755caae362e5c4fd74e74d99f15fc3b10f3cb
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:14:11 -0400

    test file to unit test head.js file function

--- test/headTest.js
+++ test/headTest.js
@@ -1,26 +1,9 @@
-
-const assert = require('chai').assert;
-const head = require('../head');
-
-describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
-  });
-  it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
-  });
-
-});
-
-
-
-
-
-
-/*
 const head = require('../head');
 const assertEqual = require('../assertEqual');
-console.log(head([1,2,3]))
+// TEST CODE
+
+console.log(head([1,2,3]));
+console.log(head([]));
+console.log(head([1]));
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-*/
