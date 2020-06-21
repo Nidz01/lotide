e5dcb8a6fea1b37a05ab69348f04218558f10f5b
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 09:16:06 -0400

    Update headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,12 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-// TEST CODE
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
 
-console.log(head([1,2,3]));
-console.log(head([]));
-console.log(head([1]));
-assertEqual(head([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-
-
+});
