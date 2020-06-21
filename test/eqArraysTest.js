68ee5ccd9579d6b392e35fac9cf97a982519b291
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:34:01 -0400

    Update eqArraysTest.js
    Test file for unti testing of eqArrays js file function

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,18 @@
+const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+
+//TEST CASES
+
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+/*Use assertEqual to write test cases for various scenarios. */
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+
+
