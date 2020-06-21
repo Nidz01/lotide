d9df2a4425221d8ad8bb86ec6d8a01f6ef723873
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:32:59 -0400

    Update eqArrays.js
    Refactored simple function with export option

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,6 @@
-/*const assertEqual = function(actual, expected) {
-  const pass = '✅️✅️✅️';
-  const fail = '❌️❌️❌️';
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-/*TEST CODE
-assertEqual("Nida", "Nida");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 5);
-*/
+
 /*Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match. */
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -27,16 +14,3 @@ const eqArrays = function(array1, array2) {
 };
 
 module.exports = eqArrays;
-/*
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-/*Use assertEqual to write test cases for various scenarios. */
-/*assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
-*/
-
