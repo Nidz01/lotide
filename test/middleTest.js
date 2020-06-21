e58d6e634dbdd655c229be62babd73a68e1000f2
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:57:03 -0400

    Update middleTest.js
    Test Case JS file for middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,9 @@
-const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// TEST CASES
 
 assertArraysEqual(middle([10, 6, 4, 2, 3]), [4]);
+assertArraysEqual(middle([10, 6, 4, 2, 3]), [5]);
+assertArraysEqual(middle([10, 6, 4, 2]), [6, 4]);
+assertArraysEqual(middle([10, 6, 4, 2]), [10, 6]);
