01aa9640e35f4c666904b2d15569c20ac068de85
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:25:08 -0400

    Update headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,13 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
+
 // TEST CODE
 
 console.log(head([1,2,3]));
 console.log(head([]));
 console.log(head([1]));
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+
