d4828aedd3434be6c3729b3c6cae63ba52dad8cf
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:41:48 -0400

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 /*
 Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
@@ -5,19 +7,7 @@ The message will be similar to that of assertEqual. In fact, you could refer to 
 implement this. Instead of doing a simple === check though, this assertion function will make use of your eqArrays 
 function for array comparison.
 */
-/*
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-*/
+
 const assertArraysEqual = function(actual, expected) {
   if(eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -27,6 +17,7 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 module.exports = assertArraysEqual;
-/*
+
+
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);*/
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
