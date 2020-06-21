b277a0c1ccb20230a9be93f75a54c25d795a5a78
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-21 08:55:15 -0400

    Update middle.js
    Refactored and simplified with export option and a separate test file

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-/* Function that checks if incoming arrays are equal and
-then return true (if equal) and false(if false) accordingly. */
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-/* Function that calls eqArrays function to check if incoming arrays are equal and
-then displays a message accoridngly. */
-
-const assertArraysEqual = function(actual, expected) {
-  if(eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️❌️❌️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 /* Implement middle which will take in an array and 
 return the middle-most element(s) of the given array. */
 
@@ -40,10 +14,4 @@ const middle = function(array) {
   }
 }
 
-assertArraysEqual(middle([10, 6, 4, 2, 3]), [4]);
-assertArraysEqual(middle([10, 6, 4, 2, 3]), [5]);
-assertArraysEqual(middle([10, 6, 4, 2]), [6, 4]);
-assertArraysEqual(middle([10, 6, 4, 2]), [10, 6]);
-
 module.exports = middle;
-
