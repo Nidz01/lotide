bfe3d42dc5d6da4e019c20ab64b52038a1548492
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 12:39:03 -0400

    countLettersTest.js
    test file for countLetters function js file

---
+++ test/countLettersTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('./assertEqual');
+const countLetters = require('../countLetters');
+
+assertEqual(countLetters('Nida is a girl'), 12);
+console.log(countLetters('Nida is a girl'));
