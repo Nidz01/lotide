3746cdc4dc0e85048d6fe023c75778399e02c240
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 13:22:07 -0400

    Update findKey.js
    export function with separate test file

--- findKey.js
+++ findKey.js
@@ -1,16 +1,3 @@
-/*
-Make the function compare the two values it takes in and print out a message telling us if they match or not. 
-Use template literals when generating the output string.
-*/
-const assertEqual = function(actual, expected) {
-  const pass = '✅️✅️✅️';
-  const fail = '❌️❌️❌️';
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 /*
 Implement the function findKey which takes in an object and a callback. 
@@ -26,25 +13,4 @@ const findKey = function(object, callback) {
   }
 }
 
-/* TEST CASE */
-
-const hills = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}
-
-assertEqual(findKey(hills, x => x.stars === 2), "noma"); // => "noma"
-
-///////////////////////////////////////////////////
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire"
-};
-
-assertEqual(findKey(bestTVShowsByGenre, x => x === "The Wire"), "drama");
+module.exports = findKey;
