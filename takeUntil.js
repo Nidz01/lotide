74f4b0da142f93733bb432b5c0d4802e08513ae6
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 13:19:05 -0400

    Update takeUntil.js
    simplified function with separate test file

--- takeUntil.js
+++ takeUntil.js
@@ -1,28 +1,3 @@
-/* Function that checks if incoming arrays are equal and
-then return true (if equal) and false(if false) accordingly. */
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-/*
-Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console. */
-
-const assertArraysEqual = function(actual, expected) {
-  if(eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌️❌️❌️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 /*
 Implement a function quite similar to this one, but isn't present in their library: takeUntil.
@@ -48,24 +23,4 @@ const takeUntil = function(array, callback) {
   return results;
 }
 
-/* TEST CASES */
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-
-const results1 = takeUntil(data1, x => x < 0);
-
-console.log(results1);
-
-assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
-
-///////////////////////////////////////////////////////////////
-console.log('---');
-///////////////////////////////////////////////////////////////
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-
-const results2 = takeUntil(data2, x => x === ',');
-
-console.log(results2);
-
-assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;
