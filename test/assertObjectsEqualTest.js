3cd8081079ac4bfd06ba2387fa0e0c7d0746d76c
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 13:11:48 -0400

    assertObjectEqualTest.js
    Test file for assertObjectEqual.js file function

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,7 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+
+/* TEST CASES */
+
+ assertObjectsEqual({a: [1]}, { a: [1]});
+ assertObjectsEqual({a: 1}, { a: [1]});
+ assertObjectsEqual({a: "1", b: 2}, {b: 2,  a: "1"});
