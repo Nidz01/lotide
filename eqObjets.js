e8043cc92af403974a1d7095c539c8dcae297aa2
Nida Q <65258819+Nidz01@users.noreply.github.com>
2020-06-22 13:22:47 -0400

    Update eqObjets.js
    simplified function with export function and separate test file

--- eqObjets.js
+++ eqObjets.js
@@ -1,31 +1,4 @@
-/*
-Make the function compare the two values it takes in and print out a message telling us if they match or not. 
-Use template literals when generating the output string.
-*/
-const assertEqual = function(actual, expected) {
-  const pass = '✅️✅️✅️';
-  const fail = '❌️❌️❌️';
-  if (actual === expected) {
-    console.log(`${pass} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${fail} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-/* Function that checks if incoming arrays are equal and
-then return true (if equal) and false(if false) accordingly. */
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 /*
 Implement the definition for function eqObjects which will take in two objects and 
@@ -48,36 +21,4 @@ const eqObjects = function(object1, object2) {
   return true;
 }
 
-/* TEST CASES */
-
- const ab = {
-   a: "1",
-   b: "2"
- };
- const ba = {
-   b: "2",
-   a: "1"
- };
- assertEqual(eqObjects(ab, ba), true);
-
- const abc = {
-   a: "1",
-   b: "2",
-   c: "3"
- };
- assertEqual(eqObjects(ab, abc), false);
- const cd = {
-   c: "1",
-   d: ["2", 3]
- };
- const dc = {
-   d: ["2", 3],
-   c: "1"
- };
- assertEqual(eqObjects(cd, dc), true);
-
- const cd2 = {
-   c: "1",
-   d: ["2", 3, 4]
- };
- assertEqual(eqObjects(cd, cd2), false);
+ module.exports = eqObjects;
